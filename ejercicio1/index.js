dfa367871a6fa9feaf056efe02ada62c4458a81a
Cristian Falcone <cristianfalcone@hotmail.com>
2024-12-09 17:51:08 -0300

    puliendo algunos detalle

--- ejercicio1/index.js
+++ ejercicio1/index.js
@@ -1,20 +1,29 @@
-const suma=document.getElementById("incrementar");
-const resta=document.getElementById("decrementar");
-let resultado=document.getElementById("contador");
-let contador=0;
+/*
+En su página web, agregar un campo para llevar un
+contador de X elemento que siempre arranque en 0
+• En la página web se muestra la cantidad de X
+• Hay un botón para incrementar la cantidad
+• Hay un botón para decrementar la cantidad
+*/
 
-function sumando(){
-    contador=contador+1;
-    resultado.innerText=contador;
+
+const suma = document.getElementById("incrementar");
+const resta = document.getElementById("decrementar");
+let resultado = document.getElementById("contador");
+let contador = 0;
+
+function sumando() {
+    contador = contador + 1;
+    resultado.innerText = contador;
 }
 
-function restando(){
-    if (contador>0){
-            contador=contador-1;
-            resultado.innerText=contador;
-                   }
+function restando() {
+    if (contador > 0) {
+        contador = contador - 1;
+        resultado.innerText = contador;
+    }
 }
 
 
-suma.addEventListener("click",sumando);
-resta.addEventListener("click",restando);
+suma.addEventListener("click", sumando);
+resta.addEventListener("click", restando);
--- ejercicio3/index.js
+++ ejercicio3/index.js
@@ -1,29 +1,38 @@
-const tirar=document.getElementById("botonTirar");
-const mensaje=document.getElementById("mensaje");
+/*
+Hacer una aplicación web que al apretar un botón
+simule el lanzamiento de dos dados 10 veces,
+sumarlos y muestre en el HTML la cantidad de
+veces que salió 7.
 
-function probarSuerte(){
- let dado1=0;
- let dado2=0;
- let sumaDados=0;
- contador=0;
- for (let i = 1; i <=10; i=i+1) {
-            dado1=Math.ceil(Math.random()*6);
-            dado2=Math.ceil(Math.random()*6);
-            console.log(dado1," ",dado2);
-            sumaDados=dado1+dado2;
-            //mensaje.innerText=" MMM, sin suerte!!!, has sacado " + sumaDados;   
-            if (dado1+dado2==7) {
-                                contador=contador+1;
-                                }
-                               }
-if (contador>0){
-                mensaje.innerText=" BIEN!!!, usted saco 7:  "+contador+" veces.";
-               } else {
-                         mensaje.innerText=" MMM, sin suerte!!!, sigue probando...";
-                      }
+*/
+
+
+const tirar = document.getElementById("botonTirar");
+const mensaje = document.getElementById("mensaje");
+
+function probarSuerte() {
+    let dado1 = 0;
+    let dado2 = 0;
+    let sumaDados = 0;
+    contador = 0;
+    for (let i = 1; i <= 10; i = i + 1) {
+        dado1 = Math.ceil(Math.random() * 6);
+        dado2 = Math.ceil(Math.random() * 6);
+        console.log(dado1, " ", dado2);
+        sumaDados = dado1 + dado2;
+        //mensaje.innerText=" MMM, sin suerte!!!, has sacado " + sumaDados;   
+        if (sumaDados == 7) {
+            contador = contador + 1;
+        }
+    }
+    if (contador > 0) {
+        mensaje.innerText = " BIEN!!!, usted saco 7:  " + contador + " veces.";
+    } else {
+        mensaje.innerText = " MMM, sin suerte!!!, sigue probando...";
+    }
 }
 
-tirar.addEventListener("click",probarSuerte);
+tirar.addEventListener("click", probarSuerte);
 
 
 
