e6ffa44b04eb7e5a58fbdfd6a51bdb57507e48a6
Cristian Falcone <cristianfalcone@hotmail.com>
2024-12-09 11:42:27 -0300

    agregado de control si es numero o letra

--- ejercicio2/index.js
+++ ejercicio2/index.js
@@ -2,13 +2,35 @@ const precios=document.getElementById("precios");
 const lista=document.getElementById("lista");
 const resultado=document.getElementById("resultado");
 let acu=0;
+
+function esNumero(cadena){
+  let numero=1;   
+  for (let i = 0; i < cadena.length; i=i+1) {
+     if ((cadena[i]=="0") || (cadena[i]=="1") || (cadena[i]=="2") || (cadena[i]=="3")|| (cadena[i]=="4") || (cadena[i]=="5")||
+     (cadena[i]=="6") || (cadena[i]=="7")||(cadena[i]=="8") || (cadena[i]=="9")||(cadena[i]==".")){
+                                                                                                  numero=1;
+                                                                                                  } else {
+                                                                                                         numero=0;
+                                                                                                         } 
+     numero=numero*numero;                                                                                                     
+     console.log(numero);
+  }
+ if (numero==1) {
+                  return true;
+                } else {
+                         return false;
+                       }
+}
+
+
 function cargarprecios(evento){
      evento.preventDefault();
      const item=document.getElementById("item").value;
-     if (item) {
+     if ((item) && (esNumero(item))){
                       const li= document.createElement("li");
                       li.innerText = item;
                       lista.appendChild(li);
+                      console.log(item);
                       acu= acu+ parseFloat(item);
                       precios.reset();
                       console.log(acu);
